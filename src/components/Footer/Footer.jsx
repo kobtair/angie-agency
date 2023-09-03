b4ad4fc72189bc36d8d0ad531bd777593618c770
Aaqib Imran <aaqibimran0@gmail.com>
2023-09-03 12:06:16 +0500

    hover underline on links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,16 +6,16 @@ export default function Footer() {
         <div className="upper-section">
           <img src="/src/assets/footer-logo.png" alt="Angie Agency" />
           <ul className="links">
-            <li className="link connect"><a href="#" target="blank">CONNECT</a></li>
-            <li className="link"><a href="#" target="blank">Linkedin</a></li>
-            <li className="link"><a href="#" target="blank">Facebook</a></li>
-            <li className="link"><a href="#" target="blank">Twitter (X)</a></li>
-            <li className="link"><a href="#" target="blank">Instagram</a></li>
+            <li className="link hover:underline connect"><a href="#" target="blank">CONNECT</a></li>
+            <li className="link hover:underline"><a href="#" target="blank">Linkedin</a></li>
+            <li className="link hover:underline"><a href="#" target="blank">Facebook</a></li>
+            <li className="link hover:underline"><a href="#" target="blank">Twitter (X)</a></li>
+            <li className="link hover:underline"><a href="#" target="blank">Instagram</a></li>
           </ul>
         </div>
         <div className="bottom-section">
           <span>Copyright 2009-2023 © Angie Agency®</span>
-          <a href="mailto:info@angieagency.com">info@angieagency.com</a>
+          <a className="hover:underline" href="mailto:info@angieagency.com">info@angieagency.com</a>
         </div>
     </FooterContainer>
   )
